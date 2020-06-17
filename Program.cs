33b1fe8e28371a410e78080520d656803e00dd5f
Bárbara <babyr.oliveira@gmail.com>
2020-06-17 16:28:42 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numerador, denominador; 
+           
+            Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(" ----- DIVISOR <3 ----- \n");
+                            
+            Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write("Digite o numerador da divisão: ");
+                            numerador = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Digite o denominador da divisão: ");
+                            denominador = Convert.ToDouble(Console.ReadLine());
+
+            if (denominador == 0) 
+            
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+                Environment.Exit (-1);
+            }
+
+            double result = numerador / denominador ;
+
+            Console.WriteLine ($"{numerador} dividido por {denominador} é {result}. ");
+        }
+    }
+}
